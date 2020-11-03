bf5d732e7a1dc17d17f259b47887f2461b51cf3c
PutuSenna <kugahunter@gmail.com>
2020-11-03 13:53:48 +0800

    Update shopAuthController.php

--- app/Http/Controllers/shopAuthController.php
+++ app/Http/Controllers/shopAuthController.php
@@ -11,7 +11,7 @@ class shopAuthController extends Controller
     public function indexRegister(){
         $shop = Shop::isExist(Session::get("id"));
         if($shop){
-            $shop1=Shop::find(Session::get("id"));
+            $shop1=Shop::get()->where("id",Session::get("id"));
            // dd($shop);
             return view('seller.dashboard',['shop1'=>$shop1]);
             //return "Welcome to seller dashboard";
@@ -28,6 +28,31 @@ class shopAuthController extends Controller
             return "Shop registration success";
         }
         return redirect()->back();
+    }
+    public function update(){
+        $shop=Shop::get()->where("id",Session::get("id"));
+        return view('seller.update',['shop'=>$shop]);
+    }
+    public function process(Request $request){
+        //$validatedData = $request->validate(Shop::getValidationRules());
+        $this->validate($request, [ "name"=>"required|unique:tb_shops|min:3|regex:/^\S*$/u",
+        "user_id"=>"required|exists:tb_users,id",
+        "description"=>"required|min:3",
+        "idcard_picture"=>"nullable|file|image|mimes:jpeg,png,jpg|max:2048",
+        "status"=>"nullable",]);
+        // store file data as variable $file
+        $picture = $request->file('picture');
+        $picture_name = time()."_".$picture->getClientOriginalName();
+        // Move file to data_file folder
+		$upload_to = 'data_file';
+        $picture->move($upload_to,$picture_name);
+
+        $shop=Shop::get()->where("id",Session::get("id"));
+        $shop->name= $request->name;
+        $shop->description=$request->description;
+        $shop->idcard_picture=$request->idcard_picture;
+
+        return view('seller.dashboard',['shop1'=>$shop]);
     }
 
 }

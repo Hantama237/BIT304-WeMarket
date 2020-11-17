0c25cd2e7e2cfa3db68bd53000a59a343c1ec28a
PutuSenna <kugahunter@gmail.com>
2020-11-17 21:13:18 +0800

    Update tb_products.php

--- app/tb_products.php
+++ app/tb_products.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class tb_products extends Model
 {
+    protected $guarded = ["id"];
+
+    //validation
+    protected static $validationRule = [
+        "shop_id"=>"required|exists:tb_shops,id",
+        "name"=>"required",
+        "description"=>"required|min:3",
+        "price"=>"required",
+        "stock"=>"required",
+        "picture"=>"nullable|file|image|mimes:jpeg,png,jpg|max:2048",
+        "status"=>"nullable",
+    ];
+    public static function getValidationRules(){
+        return self::$validationRule;
+    }
+    public static function getValidationRule($key){
+        return self::$validationRule[$key];
+    }
+
+    //method
+    public static function register($validatedData){
+        return self::insert([$validatedData]);
+    }
+    public static function isExist($userid){
+        return self::where("user_id",$userid)->first();
+    }
     //
 }

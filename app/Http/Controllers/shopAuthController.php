173e7809cba10d1b22e4af8b0c9fe6e47e3e7bdf
PutuSenna <kugahunter@gmail.com>
2020-11-23 19:38:34 +0800

    Update shopAuthController.php

--- app/Http/Controllers/shopAuthController.php
+++ app/Http/Controllers/shopAuthController.php
@@ -16,7 +16,10 @@ class shopAuthController extends Controller
         $shop = Shop::isExist(Session::get("id"));
         if($shop){
             $shop1=Shop::get()->where("id",Session::get("id"));
-           // dd($shop);
+            $shop2=Shop::where("id",Session::get("id"))->first();
+            if($shop2->status == 0){
+                return view('seller.newSeller',['shop1'=>$shop1]);    
+            }
             return view('seller.dashboard',['shop1'=>$shop1]);
             //return "Welcome to seller dashboard";
         }
@@ -77,6 +80,28 @@ class shopAuthController extends Controller
         // dd($shop->idcard_picture);
         // dd($request->name);
         //$validatedData = $request->validate(Shop::getValidationRules());
+        if($shop->status == 0){
+            $this->validate($request, [
+                "name"=>"required|min:3|regex:/^\S*$/u",
+                "user_id"=>"required|exists:tb_users,id",
+                "description"=>"required|min:3",
+                "idcard_picture"=>"nullable|file|image|mimes:jpeg,png,jpg|max:2048",
+                "status"=>"nullable",]);
+                // store file data as variable $file
+                $idcard_picture = $request->file('idcard_picture');
+                $idcard_picture_name = time()."_".$idcard_picture->getClientOriginalName();
+                // Move file to data_file folder
+                $upload_to = 'data_file';
+                $idcard_picture->move($upload_to,$idcard_picture_name);
+        
+            //    $shop->insert($request); 
+                $shop->name = $request->name;
+                $shop->description=$request->description;
+                $shop->idcard_picture=$idcard_picture_name;
+               $shop->save();
+               //Session::flash('message','Update successfully.');
+                return redirect()->back();
+        }
         $this->validate($request, [
         "name"=>"required|unique:tb_shops|min:3|regex:/^\S*$/u",
         "user_id"=>"required|exists:tb_users,id",

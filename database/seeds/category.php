48b99591cbaf5f75b3a8d27302dd3716213b3db7
PutuSenna <kugahunter@gmail.com>
2020-11-30 20:55:53 +0800

    Create category.php

---
+++ database/seeds/category.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\tb_category as Category;
+class categorySeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $categories =[
+            "vegetables",
+            "fruit",
+            "vegetables and fruit"
+        ];
+
+        foreach ($categories as $c) {
+            Category::insert(["category"=>$c]);
+        }
+    }
+}

56b96308d635fb4f076e60c24ea8be8b87c68d30
PutuSenna <kugahunter@gmail.com>
2020-12-02 17:12:58 +0800

    Create 2020_12_02_073207_tb_order.php

---
+++ database/migrations/2020_12_02_073207_tb_order.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TbOrder extends Migration
+{
+    
+    public function up()
+    {
+        Schema::create('tb_orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id");
+            $table->foreignId("shop_id");
+            $table->string("status");
+            $table->integer("total_price");
+            $table->string("payment_method");
+            $table->string("delivery_method");
+            $table->string("address");
+            $table->timestamps("order_date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_orders');
+    }
+
+
+}

3d9c38bd518a68227a0f30e54a877282cb3637f7
PutuSenna <kugahunter@gmail.com>
2020-12-02 17:13:04 +0800

    Update manageOrder.blade.php

--- resources/views/seller/manageOrder.blade.php
+++ resources/views/seller/manageOrder.blade.php
@@ -36,7 +36,7 @@
                     </ul>
                 </div>
             </div>
-            <div class="col-lg-6">
+            <div class="col-lg-9">
               @if(count($errors) > 0)
               <div class="alert alert-danger">
                 @foreach ($errors->all() as $error)
@@ -44,9 +44,44 @@
                 @endforeach
               </div>
               @endif
-                <a class="btn btn-success" href="#" role="button">Order</a>
-                <a class="btn btn-primary" href="#" role="button">Take/Delivery status</a>
+                <a class="btn btn-success" href="/seller/order" role="button">Order</a>
+                <a class="btn btn-primary" href="/seller/takeOrDelivery" role="button">Take/Delivery status</a>
                   <h3>Manage order</h3>
+               
+                  <table class="table" style="width: 100%; color:black;">
+                    <tr style="border: 1px solid gray;">
+                      <th scope="col">Order date</th>
+                      <th scope="col">Order item</th>
+                      <th scope="col">Payment Method</th>
+                      <th scope="col">Delivery method</th>
+                      <th scope="col">Total Price</th>
+                      <th scope="col">Action</th>
+                    </tr>
+                    @foreach ($order as $o)
+                    <tr style="border: 1px solid #ddd;">
+                    <td style="border: 1px solid #ddd;">{{$o->order_date}}</td>
+                      
+                        <td style="border: 1px solid #ddd;">  @foreach ($orderProduct->where("order_id",$o->id) as $item)
+                          @foreach ($product->where("id",$item->product_id) as $p)
+                          <p>Product: {{$p->name}}</p>
+                          @endforeach
+                          <p>Quantity: {{$item->quantity}}</p>
+                          <p>Price: {{$item->price}}</p>
+                          
+                      
+                        @endforeach
+                      </td>
+                    <td style="border: 1px solid #ddd;">{{$o->payment_method}}</td>
+                    <td >{{$o->delivery_method}}</td>
+                    <td >{{$o->total_price}}</td>
+                    <td style="border: 1px solid #ddd;"><a class="btn btn-primary" style="margin-top: 5px" href="/seller/processOrder/{{$o->id}}" role="button">Accept</a>
+                        <br><br><a class="btn btn-danger" href="/seller/cancelOrder/{{$o->id}}" role="button">Decline</a>
+                    </td>
+                    </tr>
+                        
+                    @endforeach
+
+                  </table>
             
               
             </div>
